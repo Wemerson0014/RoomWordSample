786924afb62237b9af5aaf40fae58ced07d5cf8e
Wemerson Fernandes Rangel <wemersonf.rangel@outlook.com>
2021-05-13 19:16:53 -0300

    Modifica retorno na interface WordDao na fun
    "Modifica retorno na interface para o metodo getAlphatizedWords e cria a
    classe WordRoomDatabase"Modifica retorno na interface para o metodo
    getAlphatizedWords e cria a
    classe WordRoomDatabase
    "Modifica retorno na interface para o metodo getAlphatizedWords e cria a
    classe WordRoomDatabase

--- app/src/main/java/com/estudo/roomwordsample/data/WordDao.kt
+++ app/src/main/java/com/estudo/roomwordsample/data/WordDao.kt
@@ -3,11 +3,12 @@ package com.estudo.roomwordsample.data
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 
 interface WordDao {
 
     @Query("SELECT * FROM word_table ORDER BY word ASC")
-    fun getAlphabetizedWords(): List<Word>
+    fun getAlphabetizedWords(): Flow<List<Word>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(word: Word)
