3d9783e30d1346bd41576ac4e1bdaec6e21c41c5
Wemerson Fernandes Rangel <wemersonf.rangel@outlook.com>
2021-05-14 19:10:23 -0300

    Cria pacote repository e a classe WordRepository

---
+++ app/src/main/java/com/estudo/roomwordsample/data/repository/WordRepository.kt
@@ -0,0 +1,17 @@
+package com.estudo.roomwordsample.data.repository
+
+import androidx.annotation.WorkerThread
+import com.estudo.roomwordsample.data.Word
+import com.estudo.roomwordsample.data.WordDao
+import kotlinx.coroutines.flow.Flow
+
+class WordRepository(private val wordDao: WordDao) {
+
+    val allWords: Flow<List<Word>> = wordDao.getAlphabetizedWords()
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun insert(word: Word) {
+        wordDao.insert(word)
+    }
+}
