434091b48e6813a153d60ec5c84742b506a12cd5
Wemerson Fernandes Rangel <wemersonf.rangel@outlook.com>
2021-05-17 19:45:49 -0300

    Adiciona classe NewWordActivity, cria a classe WordApplication, modifica a classe Word e a classe WordRoomDatabase, cria layout para a activity_new_word, cria novos recursos de string, adiciona nova dimensão e cores

--- app/src/main/java/com/estudo/roomwordsample/WordsApplication.kt
+++ app/src/main/java/com/estudo/roomwordsample/WordsApplication.kt
@@ -3,9 +3,12 @@ package com.estudo.roomwordsample
 import android.app.Application
 import com.estudo.roomwordsample.data.database.WordRoomDatabase
 import com.estudo.roomwordsample.data.repository.WordRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 
-class WordsApplication: Application() {
+class WordsApplication : Application() {
 
-    val database by lazy { WordRoomDatabase.getDatabase(this) }
+    val applicationScope = CoroutineScope(SupervisorJob())
+    val database by lazy { WordRoomDatabase.getDatabase(this, applicationScope) }
     val repository by lazy { WordRepository(database.wordDao()) }
 }
