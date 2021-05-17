1a07be09e28f430dc6260afcf88b5df1c1c7c271
Wemerson Fernandes Rangel <wemersonf.rangel@outlook.com>
2021-05-17 18:56:46 -0300

    Cria a classe WordListAdapter e cria métodos para configuração da recyclerview e adapter na classe MainActivity

--- app/src/main/java/com/estudo/roomwordsample/MainActivity.kt
+++ app/src/main/java/com/estudo/roomwordsample/MainActivity.kt
@@ -2,10 +2,21 @@ package com.estudo.roomwordsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        val adapter = WordListAdapter()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
